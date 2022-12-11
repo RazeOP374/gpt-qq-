762cc532b2165da5314a2a62cec4ebaad1afa7f5
LOVE SUISEI <87218053+Raze9@users.noreply.github.com>
2022-12-11 20:53:46 +0800

    Add files via upload

---
+++ gpt_token/gpt.go
@@ -0,0 +1,123 @@
+package gpt_token
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/satori/go.uuid"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Chat struct {
+	client       *http.Client
+	sessionToken string
+	session      *Session
+	cid          uuid.UUID
+	pid          uuid.UUID
+}
+
+func (ctx *Chat) RefreshJWT() error {
+	if !ctx.session.IsInvalid() {
+		return nil
+	}
+	req, err := http.NewRequest("GET", "https://chat.openai.com/api/auth/session", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0")
+	req.Header.Add("Cookie", "__Secure-next-auth.session-token="+ctx.sessionToken)
+	resp, err := ctx.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if err = json.NewDecoder(resp.Body).Decode(&ctx.session); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ctx *Chat) AutoRefreshJWT() {
+	go func() {
+		for {
+			ctx.RefreshJWT()
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+func (ctx *Chat) Send(text string) (*Response, error) {
+	var (
+		cid *uuid.UUID
+		pid *uuid.UUID
+	)
+	if err := ctx.RefreshJWT(); err != nil {
+		return nil, err
+	}
+	if ctx.cid != uuid.Nil {
+		cid = &ctx.cid
+	}
+	if ctx.pid == uuid.Nil {
+		tid := uuid.NewV4()
+		pid = &tid
+	} else {
+		pid = &ctx.pid
+	}
+	res, err := ctx.SendMessage(text, cid, pid)
+	if err != nil {
+		return nil, err
+	}
+	ctx.cid = res.ConversationId
+	ctx.pid = res.Message.ID
+	return res, nil
+}
+
+func (ctx *Chat) SendMessage(text string, cid, pid *uuid.UUID) (*Response, error) {
+	var chatResponse *Response
+	if err := ctx.RefreshJWT(); err != nil {
+		return nil, err
+	}
+	chatRequest := NewRequest(text, cid, pid)
+	requestBytes, err := json.Marshal(chatRequest)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", "https://chat.openai.com/backend-api/conversation", bytes.NewBuffer(requestBytes))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0")
+	req.Header.Add("Authorization", "Bearer "+ctx.session.AccessToken)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := ctx.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	responseBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	arr := strings.Split(string(responseBytes), "\n\n")
+	index := len(arr) - 3
+	if index >= len(arr) || index < 1 {
+		return nil, errors.New(string(responseBytes))
+	}
+	arr = strings.Split(arr[index], "data: ")
+	if len(arr) < 2 {
+		return nil, errors.New(string(responseBytes))
+	}
+	if err = json.Unmarshal([]byte(arr[1]), &chatResponse); err != nil {
+		return nil, err
+	}
+	return chatResponse, nil
+}
+
+func NewChat(sessionToken string) *Chat {
+	chat := &Chat{client: &http.Client{}, sessionToken: sessionToken, session: &Session{}}
+	chat.AutoRefreshJWT()
+	return chat
+}
---
+++ gpt_token/request.go
@@ -0,0 +1,41 @@
+package gpt_token
+
+import "github.com/satori/go.uuid"
+
+type REQContent struct {
+	ContentType string   `json:"content_type"`
+	Parts       []string `json:"parts"`
+}
+
+type REQMessage struct {
+	ID      uuid.UUID  `json:"id"`
+	Role    string     `json:"role"`
+	Content REQContent `json:"content"`
+}
+
+type Request struct {
+	Action          string       `json:"action"`
+	ConversationId  *uuid.UUID   `json:"conversation_id"`
+	Messages        []REQMessage `json:"messages"`
+	ParentMessageId *uuid.UUID   `json:"parent_message_id"`
+	Model           string       `json:"model"`
+}
+
+func NewRequest(text string, cid, pid *uuid.UUID) *Request {
+	return &Request{
+		Action:         "next",
+		ConversationId: cid,
+		Messages: []REQMessage{
+			{
+				ID:   uuid.NewV4(),
+				Role: "user",
+				Content: REQContent{
+					ContentType: "text",
+					Parts:       []string{text},
+				},
+			},
+		},
+		ParentMessageId: pid,
+		Model:           "text-davinci-002-render",
+	}
+}
